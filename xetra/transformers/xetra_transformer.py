58963c502dbe7b33647380926dedce2b2fd070e9
Avishek <abyss232002@gmail.com>
2022-09-20 12:20:24 -0400

    change in run.py

--- xetra/transformers/xetra_transformer.py
+++ xetra/transformers/xetra_transformer.py
@@ -8,6 +8,7 @@ import pandas as pd
 from xetra.common.s3 import S3BucketConnector
 from xetra.common.meta_process import MetaProcess
 
+
 class XetraSourceConfig(NamedTuple):
     """
     Class for source configuration data
@@ -61,6 +62,7 @@ class XetraTargetConfig(NamedTuple):
     trg_key_date_format: str
     trg_format: str
 
+
 class XetraETL():
     """
     Reads the Xetra data, transforms and writes the transformed to target
@@ -86,8 +88,8 @@ class XetraETL():
         self.trg_args = trg_args
         self.extract_date, self.extract_date_list = MetaProcess.return_date_list(
             self.src_args.src_first_extract_date, self.meta_key, self.s3_bucket_trg)
-        self.meta_update_list = [date for date in self.extract_date_list\
-            if date >= self.extract_date]
+        self.meta_update_list = [date for date in self.extract_date_list \
+                                 if date >= self.extract_date]
 
     def extract(self):
         """
@@ -97,13 +99,13 @@ class XetraETL():
           data_frame: Pandas DataFrame with the extracted data
         """
         self._logger.info('Extracting Xetra source files started...')
-        files = [key for date in self.extract_date_list\
-                     for key in self.s3_bucket_src.list_files_in_prefix(date)]
+        files = [key for date in self.extract_date_list \
+                 for key in self.s3_bucket_src.list_files_in_prefix(date)]
         if not files:
             data_frame = pd.DataFrame()
         else:
-            data_frame = pd.concat([self.s3_bucket_src.read_csv_to_df(file)\
-                for file in files], ignore_index=True)
+            data_frame = pd.concat([self.s3_bucket_src.read_csv_to_df(file) \
+                                    for file in files], ignore_index=True)
         self._logger.info('Extracting Xetra source files finished.')
         return data_frame
 
@@ -125,48 +127,48 @@ class XetraETL():
         # Removing rows with missing values
         data_frame.dropna(inplace=True)
         # Calculating opening price per ISIN and day
-        data_frame[self.trg_args.trg_col_op_price] = data_frame\
-            .sort_values(by=[self.src_args.src_col_time])\
-                .groupby([
-                    self.src_args.src_col_isin,
-                    self.src_args.src_col_date
-                    ])[self.src_args.src_col_start_price]\
-                    .transform('first')
+        data_frame[self.trg_args.trg_col_op_price] = data_frame \
+            .sort_values(by=[self.src_args.src_col_time]) \
+            .groupby([
+            self.src_args.src_col_isin,
+            self.src_args.src_col_date
+        ])[self.src_args.src_col_start_price] \
+            .transform('first')
         # Calculating closing price per ISIN and day
-        data_frame[self.trg_args.trg_col_clos_price] = data_frame\
-            .sort_values(by=[self.src_args.src_col_time])\
-                .groupby([
-                    self.src_args.src_col_isin,
-                    self.src_args.src_col_date
-                    ])[self.src_args.src_col_start_price]\
-                        .transform('last')
+        data_frame[self.trg_args.trg_col_clos_price] = data_frame \
+            .sort_values(by=[self.src_args.src_col_time]) \
+            .groupby([
+            self.src_args.src_col_isin,
+            self.src_args.src_col_date
+        ])[self.src_args.src_col_start_price] \
+            .transform('last')
         # Renaming columns
         data_frame.rename(columns={
             self.src_args.src_col_min_price: self.trg_args.trg_col_min_price,
             self.src_args.src_col_max_price: self.trg_args.trg_col_max_price,
             self.src_args.src_col_traded_vol: self.trg_args.trg_col_dail_trad_vol
-            }, inplace=True)
+        }, inplace=True)
         # Aggregating per ISIN and day -> opening price, closing price,
         # minimum price, maximum price, traded volume
         data_frame = data_frame.groupby([
             self.src_args.src_col_isin,
-            self.src_args.src_col_date], as_index=False)\
-                .agg({
-                    self.trg_args.trg_col_op_price: 'min',
-                    self.trg_args.trg_col_clos_price: 'min',
-                    self.trg_args.trg_col_min_price: 'min',
-                    self.trg_args.trg_col_max_price: 'max',
-                    self.trg_args.trg_col_dail_trad_vol: 'sum'})
+            self.src_args.src_col_date], as_index=False) \
+            .agg({
+            self.trg_args.trg_col_op_price: 'min',
+            self.trg_args.trg_col_clos_price: 'min',
+            self.trg_args.trg_col_min_price: 'min',
+            self.trg_args.trg_col_max_price: 'max',
+            self.trg_args.trg_col_dail_trad_vol: 'sum'})
         # Change of current day's closing price compared to the
         # previous trading day's closing price in %
-        data_frame[self.trg_args.trg_col_ch_prev_clos] = data_frame\
-            .sort_values(by=[self.src_args.src_col_date])\
-                .groupby([self.src_args.src_col_isin])[self.trg_args.trg_col_op_price]\
-                    .shift(1)
+        data_frame[self.trg_args.trg_col_ch_prev_clos] = data_frame \
+            .sort_values(by=[self.src_args.src_col_date]) \
+            .groupby([self.src_args.src_col_isin])[self.trg_args.trg_col_op_price] \
+            .shift(1)
         data_frame[self.trg_args.trg_col_ch_prev_clos] = (
-            data_frame[self.trg_args.trg_col_op_price] \
-            - data_frame[self.trg_args.trg_col_ch_prev_clos]
-            ) / data_frame[self.trg_args.trg_col_ch_prev_clos ] * 100
+                                                                 data_frame[self.trg_args.trg_col_op_price]
+                                                                 - data_frame[self.trg_args.trg_col_ch_prev_clos]
+                                                         ) / data_frame[self.trg_args.trg_col_ch_prev_clos] * 100
         # Rounding to 2 decimals
         data_frame = data_frame.round(decimals=2)
         # Removing the day before extract_date
